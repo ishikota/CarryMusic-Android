1d44bacb07113fdd95670beb4065271f672e6255
Kota Ishimoto <ishikota086@gmail.com>
2016-08-17 12:16:43 +0900

    Add download function on MusicListFragment

--- app/src/main/java/jp/carrymusic/ui/MusicListFragment.java
+++ app/src/main/java/jp/carrymusic/ui/MusicListFragment.java
@@ -11,12 +11,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.File;
+
 import io.realm.Realm;
 import jp.carrymusic.R;
 import jp.carrymusic.api.CarryMusicApiClient;
 import jp.carrymusic.databinding.FragmentMusicListBinding;
 import jp.carrymusic.model.MusicProviderSource;
 import jp.carrymusic.utils.DividerItemDecoration;
+import jp.carrymusic.utils.FileSaveHelper;
+import retrofit.client.Response;
 import rx.Subscriber;
 
 public class MusicListFragment extends Fragment implements MusicListAdapter.MusicListClickListener {
@@ -76,6 +80,45 @@ public class MusicListFragment extends Fragment implements MusicListAdapter.Musi
                 });
     }
 
+    private void downloadVideoToDevice(final String videoId) {
+        CarryMusicApiClient.getInstance().downloadVideo(videoId).subscribe(new Subscriber<Response>() {
+            @Override
+            public void onCompleted() {
+                Log.d("MusicListFragment", "downloadVideoToDevice:Completed");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onNext(Response response) {
+                FileSaveHelper.saveMusic(getContext(), videoId, response,
+                        new FileSaveHelper.SaveMusicCallback() {
+                    @Override
+                    public void onSuccess(final File destFile) {
+                        Log.d("MusicListFragment", "downloadVideoToDevice:Save success");
+                        Realm.getDefaultInstance().executeTransaction(new Realm.Transaction() {
+                            @Override
+                            public void execute(Realm realm) {
+                                MusicProviderSource model =
+                                        realm.where(MusicProviderSource.class)
+                                                .equalTo("videoId", videoId).findFirst();
+                                model.setVideoPath(destFile.getAbsolutePath());
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onError(String message) {
+                        Log.e("MusicListFragment", "downloadVideoToDevice:" + message);
+                    }
+                });
+            }
+        });
+    }
+
     private void showProgress(boolean show) {
         Log.i("hoge", "progress:" + show);
         binding.btn.setVisibility(show ? View.GONE : View.VISIBLE);
@@ -85,5 +128,8 @@ public class MusicListFragment extends Fragment implements MusicListAdapter.Musi
 
     @Override
     public void onMusicSelected(MusicProviderSource model) {
+        if (model.getVideoPath() == null) {
+            downloadVideoToDevice(model.getVideoId());
+        }
     }
 }
