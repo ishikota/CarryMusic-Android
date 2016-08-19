321377927f70031bb46c9ba9fcd5e3b5389379fb
Kota Ishimoto <ishikota086@gmail.com>
2016-08-19 17:51:32 +0900

    Add SearchView on AppBar

--- app/src/main/java/jp/carrymusic/ui/MainActivity.java
+++ app/src/main/java/jp/carrymusic/ui/MainActivity.java
@@ -3,11 +3,18 @@ package jp.carrymusic.ui;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import jp.carrymusic.R;
 
 public class MainActivity extends BaseActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,5 +44,48 @@ public class MainActivity extends BaseActivity {
         return (MusicListFragment) getSupportFragmentManager()
                 .findFragmentByTag(MusicListFragment.class.getSimpleName());
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+
+        // Define the listener
+        MenuItemCompat.OnActionExpandListener expandListener = new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                Log.d(TAG, "onMenuItemActionCollapse with item: " + item);
+                return true;  // Return true to collapse action view
+            }
+
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                Log.d(TAG, "onMenuItemActionExpand with item: " + item);
+                // Do something when expanded
+                return true;  // Return true to expand action view
+            }
+        };
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d(TAG, "onQueryTextSubmit with query : " + query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d(TAG, "onQueryTextChange with newText : " + newText);
+                return false;
+            }
+        });
+
+        // Assign the listener to that action item
+        MenuItemCompat.setOnActionExpandListener(searchItem, expandListener);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
 
 }
