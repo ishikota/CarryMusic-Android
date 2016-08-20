3ac90222fdc3fcc960c9fa06f8990e0e5b5b8813
Kota Ishimoto <ishikota086@gmail.com>
2016-08-20 18:33:57 +0900

    Move toolbar menu logic to presenter

--- app/src/main/java/jp/carrymusic/ui/MainActivity.java
+++ app/src/main/java/jp/carrymusic/ui/MainActivity.java
@@ -3,24 +3,17 @@ package jp.carrymusic.ui;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 
 import jp.carrymusic.R;
-import jp.carrymusic.utils.DownloadHelper;
 
 public class MainActivity extends BaseActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    // Hold query while network access to restore when network access failed
-    private String mQueryMemo = "";
-
-    private SearchView mSearchView;
+    SearchMenuPresenter searchMenuPresenter = new SearchMenuPresenter();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -56,95 +49,9 @@ public class MainActivity extends BaseActivity {
         Log.d(TAG, "onCreateOptionsMenu");
 
         getMenuInflater().inflate(R.menu.main_activity_actions, menu);
-
-        // Setup progress item
-        final MenuItem progressItem = menu.findItem(R.id.action_progress);
-        MenuItemCompat.setActionView(progressItem, R.layout.menu_progress);
-
-        // Setup search item
-        final MenuItem searchItem = menu.findItem(R.id.action_search);
-        mSearchView = (SearchView) MenuItemCompat.getActionView(searchItem);
-        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                Log.d(TAG, "onQueryTextSubmit with query : " + query);
-                saveQuery(query);
-                showProgressAction(searchItem, progressItem);
-                DownloadHelper.fetchNewItem(query, new DownloadHelper.DownloadCallback() {
-                    @Override
-                    public void onSuccess() {
-                        hideProgressAction(searchItem, progressItem);
-                        clearQuery();
-                    }
-
-                    @Override
-                    public void onError(String message) {
-                        // TODO notify error in some way
-                        hideProgressAction(searchItem, progressItem);
-                        searchItem.expandActionView();
-                        restoreSearchQuery();
-                    }
-                });
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                Log.d(TAG, "onQueryTextChange with newText : " + newText);
-                return false;
-            }
-        });
-
-        // Assign the listener to that action item
-        MenuItemCompat.setOnActionExpandListener(searchItem, new MenuItemCompat.OnActionExpandListener() {
-            @Override
-            public boolean onMenuItemActionExpand(MenuItem item) {
-                restoreSearchQuery();
-                return true;
-            }
-
-            @Override
-            public boolean onMenuItemActionCollapse(MenuItem item) {
-                String query = ((SearchView) item.getActionView()).getQuery().toString();
-                saveQuery(query);
-                return true;
-            }
-        });
+        searchMenuPresenter.setup(menu);
 
         return super.onCreateOptionsMenu(menu);
-    }
-
-    private void showProgressAction(MenuItem search, MenuItem progress) {
-        search.collapseActionView();
-        search.setVisible(false);
-        progress.setVisible(true);
-    }
-
-    private void hideProgressAction(MenuItem search, MenuItem progress) {
-        search.setVisible(true);
-        progress.setVisible(false);
-    }
-
-    private void saveQuery(String query) {
-        if (!query.isEmpty()) {
-            mQueryMemo = query;
-        }
-    }
-
-    private void clearQuery() {
-        mQueryMemo = "";
-    }
-
-    private void restoreSearchQuery() {
-        mSearchView.post(new Runnable() {
-            @Override
-            public void run() {
-                if (!mQueryMemo.isEmpty()) {  // Dirty solution to handle empty string overwrite
-                    mSearchView.setQuery(mQueryMemo, false);
-                    mQueryMemo = "";
-                }
-            }
-        });
     }
 
 
---
+++ app/src/main/java/jp/carrymusic/ui/SearchMenuPresenter.java
@@ -0,0 +1,124 @@
+package jp.carrymusic.ui;
+
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import jp.carrymusic.R;
+import jp.carrymusic.utils.DownloadHelper;
+
+public class SearchMenuPresenter {
+
+    private static final String TAG = SearchMenuPresenter.class.getSimpleName();
+
+    // Hold query while network access to restore when network access failed
+    private String mQueryMemo = "";
+
+    private DownloadHelper.DownloadCallback mCallback;
+
+    public void setup(Menu menu) {
+        MenuItem progressItem = menu.findItem(R.id.action_progress);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+
+        setupProgress(progressItem);
+        setupSearchAction(searchItem, progressItem);
+    }
+
+    public void setCallback(DownloadHelper.DownloadCallback callback) {
+        this.mCallback = callback;
+    }
+
+    private void setupProgress(MenuItem progressItem) {
+        MenuItemCompat.setActionView(progressItem, R.layout.menu_progress);
+    }
+
+    private void setupSearchAction(final MenuItem searchItem, final MenuItem progressItem) {
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d(TAG, "onQueryTextSubmit with query : " + query);
+                saveQuery(query);
+                showProgressAction(searchItem, progressItem);
+                DownloadHelper.fetchNewItem(query, new DownloadHelper.DownloadCallback() {
+                    @Override
+                    public void onSuccess() {
+                        hideProgressAction(searchItem, progressItem);
+                        clearQuery();
+                        if (mCallback != null) {
+                            mCallback.onSuccess();
+                        }
+                    }
+
+                    @Override
+                    public void onError(String message) {
+                        hideProgressAction(searchItem, progressItem);
+                        searchItem.expandActionView();
+                        restoreSearchQuery(searchView);
+                        if (mCallback != null) {
+                            mCallback.onError(message);
+                        }
+                    }
+                });
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d(TAG, "onQueryTextChange with newText : " + newText);
+                return false;
+            }
+        });
+
+        MenuItemCompat.setOnActionExpandListener(searchItem, new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                restoreSearchQuery(searchView);
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                String query = ((SearchView) item.getActionView()).getQuery().toString();
+                saveQuery(query);
+                return true;
+            }
+        });
+    }
+
+    private void showProgressAction(MenuItem search, MenuItem progress) {
+        search.collapseActionView();
+        search.setVisible(false);
+        progress.setVisible(true);
+    }
+
+    private void hideProgressAction(MenuItem search, MenuItem progress) {
+        search.setVisible(true);
+        progress.setVisible(false);
+    }
+
+    private void saveQuery(String query) {
+        if (!query.isEmpty()) {
+            mQueryMemo = query;
+        }
+    }
+
+    private void clearQuery() {
+        mQueryMemo = "";
+    }
+
+    private void restoreSearchQuery(final SearchView searchView) {
+        searchView.post(new Runnable() {
+            @Override
+            public void run() {
+                if (!mQueryMemo.isEmpty()) {  // Dirty solution to handle empty string overwrite
+                    searchView.setQuery(mQueryMemo, false);
+                    mQueryMemo = "";
+                }
+            }
+        });
+    }
+
+}
